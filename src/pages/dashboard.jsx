dbbc82f0c465778291316a382c80c3d6af4192ea
vraj1763@gmail.com <vraj1763@gmail.com>
2024-01-26 22:34:13 +0530

    Improved dashboard UI, added income module and total card

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,80 +1,229 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Sidebar from "../components/Sidebar";
-import { ExpenseForm } from "../components/Dashboard/ExpenseForm";
-import { IncomeForm } from "../components/Dashboard/IncomeForm";
+import "../scss/Dashboard.scss";
 
 export const Dashboard = () => {
   const [expenses, setExpenses] = useState([]);
   const [incomes, setIncomes] = useState([]);
+  const [expenseData, setExpenseData] = useState({
+    description: "",
+    amount: 0,
+  });
+  const [incomeData, setIncomeData] = useState({
+    description: "",
+    amount: 0,
+  });
 
-  const addExpense = async (newExpense) => {
+  const addExpense = async () => {
     try {
-      // Send a request to your backend to add the expense
+      if (!expenseData.description || !expenseData.amount) {
+        console.error("Description and amount are required for expenses.");
+        return;
+      }
+
       await axios.post(
         "http://localhost:3000/api/expenses/addExpense",
-        newExpense
+        expenseData
       );
+      fetchExpenses();
+      setExpenseData({
+        description: "",
+        amount: 0,
+      });
+    } catch (error) {
+      console.error("Error adding expense", error.response.data);
+    }
+  };
 
-      // Fetch the updated list of expenses
+  const addIncome = async () => {
+    try {
+      if (!incomeData.description || !incomeData.amount) {
+        console.error("Description and amount are required for incomes.");
+        return;
+      }
+
+      await axios.post(
+        "http://localhost:3000/api/incomes/addIncome",
+        incomeData
+      );
+      fetchIncomes();
+      setIncomeData({
+        description: "",
+        amount: 0,
+      });
+    } catch (error) {
+      console.error("Error adding income", error.response.data);
+    }
+  };
+
+  const fetchExpenses = async () => {
+    try {
       const response = await axios.get(
         "http://localhost:3000/api/expenses/getExpenses"
       );
       setExpenses(response.data);
     } catch (error) {
-      console.error("Error adding expense", error);
-      // Handle error
+      console.error("Error fetching expenses", error);
     }
   };
 
-  const addIncome = (income) => {
-    setIncomes([...incomes, income]);
+  const fetchIncomes = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/incomes/getIncomes"
+      );
+      setIncomes(response.data);
+    } catch (error) {
+      console.error("Error fetching incomes", error);
+    }
   };
 
   useEffect(() => {
-    // Fetch expenses when the component mounts
-    const fetchExpenses = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3000/api/expenses/getExpenses"
-        );
-        setExpenses(response.data);
-      } catch (error) {
-        console.error("Error fetching expenses", error);
-        // Handle error
-      }
-    };
-
     fetchExpenses();
-  }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
+    fetchIncomes();
+  }, []);
+
+  const handleExpenseChange = (e) => {
+    setExpenseData({
+      ...expenseData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleIncomeChange = (e) => {
+    setIncomeData({
+      ...incomeData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const calculateTotal = (items) => {
+    return items.reduce((total, item) => total + item.amount, 0);
+  };
 
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 bg-gray-200 p-8">
+      <div className="flex-1 bg-gray-200 p-8 overflow-y-auto hide-scrollbar">
         <h1 className="text-2xl font-bold mb-4">Main Content</h1>
 
-        {/* Display Expenses */}
-        {expenses.map((expense, index) => (
-          <div key={index}>
-            <p>Description: {expense.description}</p>
-            <p>Amount: {expense.amount}</p>
-          </div>
-        ))}
-        {/* ... */}
-
-        {/* Display Incomes */}
-        <div>
-          <h2 className="text-lg font-semibold mb-2">Incomes</h2>
-          <ul>
-            {incomes.map((income, index) => (
-              <li key={index}>{income}</li>
+        {/* Display Cards */}
+        <div className="flex space-x-4 mb-4">
+          {/* Expense Card */}
+          <div className="flex-1 bg-white p-4 rounded-md shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Expenses</h2>
+            {expenses.map((expense, index) => (
+              <div key={index} className="mb-4 border-b border-gray-300 pb-2">
+                <p className="text-sm font-semibold">
+                  Description: {expense.description}
+                </p>
+                <p className="text-sm">Amount: {expense.amount}</p>
+              </div>
             ))}
-          </ul>
+          </div>
+
+          {/* Income Card */}
+          <div className="flex-1 bg-white p-4 rounded-md shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Incomes</h2>
+            <ul>
+              {incomes.map((income, index) => (
+                <li key={index} className="mb-4 border-b border-gray-300 pb-2">
+                  <p className="text-sm font-semibold">
+                    Description: {income.description}
+                  </p>
+                  <p className="text-sm">Amount: {income.amount}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Total Card */}
+          <div className="flex-1 bg-white p-4 rounded-md shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Total</h2>
+            <p className="text-sm">Total Income: {calculateTotal(incomes)}</p>
+            <p className="text-sm">Total Expense: {calculateTotal(expenses)}</p>
+            <p className="text-sm font-semibold">
+              Difference: {calculateTotal(incomes) - calculateTotal(expenses)}
+            </p>
+          </div>
         </div>
 
-        <ExpenseForm addExpense={addExpense} />
-        <IncomeForm addIncome={addIncome} />
+        {/* Expense Form */}
+        <div className="bg-white p-4 mb-4 rounded-md shadow-md">
+          <h2 className="text-lg font-semibold mb-2">Add Expense</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              addExpense();
+            }}
+          >
+            <label className="block mb-2">
+              Description:
+              <input
+                type="text"
+                name="description"
+                value={expenseData.description}
+                onChange={handleExpenseChange}
+                className="border border-gray-300 p-2 w-full rounded-md"
+              />
+            </label>
+            <label className="block mb-2">
+              Amount:
+              <input
+                type="number"
+                name="amount"
+                value={expenseData.amount}
+                onChange={handleExpenseChange}
+                className="border border-gray-300 p-2 w-full rounded-md"
+              />
+            </label>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Add Expense
+            </button>
+          </form>
+        </div>
+
+        {/* Income Form */}
+        <div className="bg-white p-4 mb-4 rounded-md shadow-md">
+          <h2 className="text-lg font-semibold mb-2">Add Income</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              addIncome();
+            }}
+          >
+            <label className="block mb-2">
+              Description:
+              <input
+                type="text"
+                name="description"
+                value={incomeData.description}
+                onChange={handleIncomeChange}
+                className="border border-gray-300 p-2 w-full rounded-md"
+              />
+            </label>
+            <label className="block mb-2">
+              Amount:
+              <input
+                type="number"
+                name="amount"
+                value={incomeData.amount}
+                onChange={handleIncomeChange}
+                className="border border-gray-300 p-2 w-full rounded-md"
+              />
+            </label>
+            <button
+              type="submit"
+              className="bg-green-500 text-white px-4 py-2 rounded-md"
+            >
+              Add Income
+            </button>
+          </form>
+        </div>
       </div>
 
       <div className="w-80 bg-white p-8">
